55d4194527494ea0924c590a036357be7cfe338d
Felipe Moraes <felipealvesdemoraes@gmail.com>
2018-04-14 21:59:52 -0300

    Minor refactor

--- src/main/java/br/com/beveragesuggester/control/TemperatureService.java
+++ src/main/java/br/com/beveragesuggester/control/TemperatureService.java
@@ -1,12 +1,14 @@
 package br.com.beveragesuggester.control;
 
 import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonPointer;
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 
 /**
  *
@@ -14,13 +16,21 @@ import javax.ws.rs.client.ClientBuilder;
  */
 public class TemperatureService {
 
-    public String getTemperature(String city) {
+    private WebTarget target;
+
+    @PostConstruct
+    public void init() {
         Client client = ClientBuilder.newBuilder()
                 .connectTimeout(500, TimeUnit.MICROSECONDS)
                 .readTimeout(700, TimeUnit.MICROSECONDS)
                 .build();
+        
+        this.target = client.target("http://api.openweathermap.org/data/2.5/weather");
+    }
+    public String getTemperature(String city) {
+        
         try {
-            JsonObject temperatureDTO = client.target("http://api.openweathermap.org/data/2.5/weather")
+            JsonObject temperatureDTO = target
                     .queryParam("q", city)
                     .queryParam("units", "metric")
                     .queryParam("APPID", "38d971ae945af9269aafe5e7352e56d5")
