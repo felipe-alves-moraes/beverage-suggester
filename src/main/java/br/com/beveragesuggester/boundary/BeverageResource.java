ce617e0e15865d1a010c64dff0735aa760b1da91
Felipe Alves de Moraes <felipealvesdemoraes@gmail.com>
2021-12-24 08:43:37 +0100

    fix: add media type

--- src/main/java/br/com/beveragesuggester/boundary/BeverageResource.java
+++ src/main/java/br/com/beveragesuggester/boundary/BeverageResource.java
@@ -4,7 +4,9 @@ import br.com.beveragesuggester.entity.Beverage;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -17,6 +19,7 @@ public class BeverageResource {
     private BeverageService beverageService;
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Beverage random(
             @QueryParam("useTemperature") boolean useTemperature) {
         if (useTemperature) {
