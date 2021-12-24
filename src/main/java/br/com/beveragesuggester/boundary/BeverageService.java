3b7ea759b535a8c7389ebdb816ecdf21951ee6e4
Felipe Alves de Moraes <felipealvesdemoraes@gmail.com>
2021-12-24 07:58:49 +0100

    refactor: JakartaEE 9.1

--- src/main/java/br/com/beveragesuggester/boundary/BeverageService.java
+++ src/main/java/br/com/beveragesuggester/boundary/BeverageService.java
@@ -3,48 +3,46 @@ package br.com.beveragesuggester.boundary;
 import br.com.beveragesuggester.control.TemperatureService;
 import br.com.beveragesuggester.entity.Beverage;
 import br.com.beveragesuggester.entity.Category;
-import java.util.Arrays;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
 import java.util.List;
-import java.util.Random;
-import static java.util.stream.Collectors.toList;
-import javax.ejb.Stateless;
-import javax.inject.Inject;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
- *
  * @author Felipe
  */
-@Stateless
+@ApplicationScoped
 public class BeverageService {
-    
-    private static final List<Beverage> BEVERAGES = Arrays.asList(
-            new Beverage("Coffee", Arrays.asList(Category.HOT, Category.ANYTIME)),
-            new Beverage("Beer", Arrays.asList(Category.COLD)),
-            new Beverage("Hot Chocolate", Arrays.asList(Category.HOT)),
-            new Beverage("Smoothie", Arrays.asList(Category.COLD, Category.ANYTIME)),
-            new Beverage("Tea", Arrays.asList(Category.HOT)));
-    
+
+    private static final List<Beverage> BEVERAGES = List.of(
+            new Beverage("Coffee", List.of(Category.HOT, Category.ANYTIME)),
+            new Beverage("Beer", List.of(Category.COLD)),
+            new Beverage("Hot Chocolate", List.of(Category.HOT)),
+            new Beverage("Smoothie", List.of(Category.COLD, Category.ANYTIME)),
+            new Beverage("Tea", List.of(Category.HOT)));
+
     @Inject
     private TemperatureService temperatureService;
-    
+
     public Beverage pickRandom() {
-        int randomIndex = new Random().nextInt(BEVERAGES.size());
-        
+        final var randomIndex = getRandomIndex(BEVERAGES.size());
+
         return BEVERAGES.get(randomIndex);
     }
-    
+
     public Beverage pickRandomBasedOnTemperature() {
-        Double temperature = temperatureService.getTemperature("Sao Paulo, BR");
-        Category bestCategoryFromTemperature = getBestCategoryFromTemperature(temperature.intValue());
-        
-        List<Beverage> beveragesTempBased = BEVERAGES.stream()
+        final var temperature = temperatureService.getTemperature("Sao Paulo, BR");
+        final var bestCategoryFromTemperature = getBestCategoryFromTemperature(temperature.intValue());
+
+        final var beveragesTempBased = BEVERAGES.stream()
                 .filter(beverage -> beverage.getCategories().contains(bestCategoryFromTemperature))
-                .collect(toList());
-        
-        int randomIndex = new Random().nextInt(beveragesTempBased.size());
+                .toList();
+
+        final var randomIndex = getRandomIndex(beveragesTempBased.size());
         return beveragesTempBased.get(randomIndex);
     }
-    
+
     private Category getBestCategoryFromTemperature(int temperature) {
         if (temperature <= 20) {
             return Category.HOT;
@@ -53,5 +51,9 @@ public class BeverageService {
         } else {
             return Category.COLD;
         }
+    }
+
+    private int getRandomIndex(int bound) {
+        return ThreadLocalRandom.current().nextInt(bound);
     }
 }
